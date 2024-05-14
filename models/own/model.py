Fix neuron selection backprop

models/own/model.py
@@ -12,7 +12,6 @@ from torch import nn
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
 from classes import BaseModelImpl
-from layers.bonito import BonitoLSTM
 from selection_lstm import SelectionLstm
 
 class OwnModel(BaseModelImpl):
@@ -140,31 +139,32 @@ class OwnModel(BaseModelImpl):
             p = self.forward(x) # forward through the network
             loss, losses = self.calculate_loss(y, p)
 
-        lambda_1 = 0.000005 # 0.08/N (if N is correctly assumed to be 384) is about 0.0002
-        lambda_2 = 0.000005
+        lambda_1 = 0.00005 # 0.08/N (if N is correctly assumed to be 384) is about 0.0002
+        lambda_2 = 0.00005
 
         log_alpha_z = None
         log_alpha_s = None
         for name, param in self.named_parameters():
             if 'log_alpha_z' in name:
-                log_alpha_z = param.clamp(0, 1)
+                log_alpha_z = param
 
             if 'log_alpha_s' in name:
-                log_alpha_s = param.clamp(0, 1)
-                loss += lambda_2 * self._nonzero_cdf(log_alpha_s).sum().item()
+                log_alpha_s = param
+                # Multiply with lambda before summing to prevent overflow
+                loss += (lambda_2 * self._nonzero_cdf(log_alpha_s)).sum()
                 
-                einsum = torch.einsum('i,j->ij',
+                # Multiply with lambda before summing to prevent overflow
+                loss += (lambda_2 * torch.einsum('i,j->ij',
                                       self._nonzero_cdf(log_alpha_s),
                                       self._nonzero_cdf(log_alpha_s)
-                                      ).fill_diagonal_(0).sum()
-                loss += lambda_2 * (einsum.item() if einsum.isfinite() else 65000)
+                                      )).fill_diagonal_(0).sum()
 
             if 'log_alpha' in name and log_alpha_z != None and log_alpha_s != None:
-                einsum = torch.einsum('i,j->ij',
+                # Multiply with lambda before summing to prevent overflow
+                loss += (lambda_1 * torch.einsum('i,j->ij',
                                       self._nonzero_cdf(log_alpha_z),
                                       self._nonzero_cdf(log_alpha_s)
-                                      ).sum()
-                loss += lambda_1 * (einsum.item() if einsum.isfinite() else 65000)
+                                      )).sum()
 
                 log_alpha_z = None
                 log_alpha_s = None

scripts/train_original.py
@@ -127,7 +127,6 @@ if __name__ == '__main__':
         num_workers = 1
     )
 
-
     if args.use_scaler:
         use_amp = True
         scaler = torch.cuda.amp.GradScaler(enabled=use_amp) # type: ignore
@@ -268,8 +267,6 @@ if __name__ == '__main__':
                 # write results to console
                 print(log_df)
                 if args.model == 'own':
-                    # nonzero_s = [torch.count_nonzero(model.encoder[l].rnn.get_parameter('s')) for l in range(3)]
-                    # nonzero_z = [torch.count_nonzero(model.encoder[l].rnn.get_parameter('z')) for l in range(3)]
                     for l in range(3):
                         for name, weight in model.encoder[l].named_parameters():
                             if not 'log_alpha' in name:
@@ -281,7 +278,7 @@ if __name__ == '__main__':
                                 print('WARNING: NaN gradients:')
                                 print(weight.grad)
                             var, mean = torch.var_mean(weight.data)
-                            # print('{}.{} [ var: {:.3}, mean: {:.3}, w0-2: {}, grd0-2: {} ]'.format(name, l, var.item(), mean.item(), weight.data[0:3].tolist(), weight.grad[0:3].tolist()))
+                            print('{}.{} [ var: {:.3}, mean: {:.3}, nonz0-2: {}, w0-2: {} ]'.format(name, l, var.item(), mean.item(), torch.sigmoid(torch.add(weight.data, 0.6942))[0:3].tolist(), weight.data[0:3].tolist()))
                 
     
     model.save(os.path.join(checkpoints_dir, 'checkpoint_' + str(total_num_steps) + '.pt'))